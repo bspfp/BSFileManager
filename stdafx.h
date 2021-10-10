68805b15da7a2cf56c492f54a58bdac52c4f26a7
bspfp <kwanghochoi@hotmail.com>
2021-10-10 16:06:30 +0900

    WM_APP 관련 수정

--- stdafx.h
+++ stdafx.h
@@ -38,20 +38,20 @@
 
 #include "resource.h"
 
-#define WM_CHANGED_FOLDER_VIEW			(WM_APP +  0)		// LPARAM: PCIDLIST_ABSOLUTE
-#define WM_VIEW_SEL_CHANGED				(WM_APP +  1)		// WPARAM: view index
-#define WM_VIEW_COMMAND					(WM_APP +  2)		// WPARAM: AddressBar::Command
-#define WM_VIEW_SET_VIEWMODE			(WM_APP +  3)		//
-#define WM_DRIVE_CHANGED				(WM_APP +  4)		// WPARAM: HANDLE		LPARAM: DWORD
-#define WM_SHORTCUT_COMMAND				(WM_APP +  5)		// LPARAM: ShortcutInfo*
-#define WM_RESET_BOOKMARK_MENU			(WM_APP +  6)		//
-#define WM_SET_TITLE					(WM_APP +  7)		// WPARAM: const wchar_t*
-#define WM_GET_SELECTED_VIEW_INDEX		(WM_APP +  8)		// RETURN: int
-#define WM_ACTIVATE_BSFM				(WM_APP +  9)		//
-#define WM_FIND_FILE_END				(WM_APP + 10)		// 
-#define WM_FIND_FILE_NOT_FOUND			(WM_APP + 11)		// 
-#define WM_FIND_FILE_RESULT				(WM_APP + 12)		// 
-#define WM_FIND_FILE_GOTO				(WM_APP + 13)		// WPARAM: const wchar_t*	LPARAM: const wchar_t*
+#define WM_CHANGED_FOLDER_VIEW			(WM_APP + 100)		// LPARAM: PCIDLIST_ABSOLUTE
+#define WM_VIEW_SEL_CHANGED				(WM_APP + 101)		// WPARAM: view index
+#define WM_VIEW_COMMAND					(WM_APP + 102)		// WPARAM: AddressBar::Command
+#define WM_VIEW_SET_VIEWMODE			(WM_APP + 103)		//
+#define WM_DRIVE_CHANGED				(WM_APP + 104)		// WPARAM: HANDLE		LPARAM: DWORD
+#define WM_SHORTCUT_COMMAND				(WM_APP + 105)		// LPARAM: ShortcutInfo*
+#define WM_RESET_BOOKMARK_MENU			(WM_APP + 106)		//
+#define WM_SET_TITLE					(WM_APP + 107)		// WPARAM: const wchar_t*
+#define WM_GET_SELECTED_VIEW_INDEX		(WM_APP + 108)		// RETURN: int
+#define WM_ACTIVATE_BSFM				(WM_APP + 109)		//
+#define WM_FIND_FILE_END				(WM_APP + 110)		// 
+#define WM_FIND_FILE_NOT_FOUND			(WM_APP + 111)		// 
+#define WM_FIND_FILE_RESULT				(WM_APP + 112)		// 
+#define WM_FIND_FILE_GOTO				(WM_APP + 113)		// WPARAM: const wchar_t*	LPARAM: const wchar_t*
 
 #define BSFM_SHORTCUT_BAR_COUNT			5
 
